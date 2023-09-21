898e9945d45ca4794f2e439bcba4d9ab25378231
rameshgagal80 <140626805+rameshgagal80@users.noreply.github.com>
2023-09-21 13:08:35 +0530

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,70 +1,114 @@
 document.addEventListener('DOMContentLoaded', function () {
-            const app = document.getElementById('app');
-            const toaster = document.getElementById('toaster');
-            const loginModal = document.getElementById('loginModal');
-            const modalUsername = document.getElementById('modalUsername');
-            const modalPassword = document.getElementById('modalPassword');
-            const closeModalButton = document.getElementById('closeModal');
 
-            // Function to create and display the login form
-            function displayLoginForm() {
-                const loginForm = document.createElement('form');
-                loginForm.id = 'loginForm';
+        // Create the button
+        const button = document.createElement('button');
+        button.id = 'showModalBtn';
+        button.textContent = 'Show Modal';
 
-                const usernameInput = document.createElement('input');
-                usernameInput.type = 'text';
-                usernameInput.placeholder = 'Username';
-                usernameInput.name = 'username';
-                usernameInput.required = true;
+        // Create the modal
+        const modal = document.createElement('div');
+        modal.id = 'modal';
+        modal.style.display = 'none';
+        modal.style.position = 'fixed';
+        modal.style.top = '0';
+        modal.style.left = '0';
+        modal.style.width = '100%';
+        modal.style.height = '100%';
+        modal.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+        modal.style.justifyContent = 'center';
+        modal.style.alignItems = 'center';
+        modal.style.zIndex = '1000';
+        modal.innerHTML = `
+            <div style="background-color: white; padding: 20px; border-radius: 5px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);">
+                <h2>Modal Content</h2>
+                <p>This is a modal dialog.</p>
+                <button id="closeModalBtn">Close</button>
+            </div>
+        `;
 
-                const passwordInput = document.createElement('input');
-                passwordInput.type = 'password';
-                passwordInput.placeholder = 'Password';
-                passwordInput.name = 'password';
-                passwordInput.required = true;
+        // Create the toaster
+        const toaster = document.createElement('div');
+        toaster.id = 'toaster';
+        toaster.style.display = 'none';
+        toaster.style.position = 'fixed';
+        toaster.style.top = '10px';
+        toaster.style.right = '10px';
+        toaster.style.backgroundColor = '#333';
+        toaster.style.color = 'white';
+        toaster.style.padding = '10px';
+        toaster.style.borderRadius = '5px';
+        toaster.style.zIndex = '1000';
+        toaster.textContent = 'Toaster Message';
 
-                const submitButton = document.createElement('button');
-                submitButton.type = 'submit';
-                submitButton.textContent = 'Login';
+        // Create the login form
+        const loginForm = document.createElement('form');
+        loginForm.id = 'loginForm';
 
-                loginForm.appendChild(usernameInput);
-                loginForm.appendChild(passwordInput);
-                loginForm.appendChild(submitButton);
+        const usernameInput = document.createElement('input');
+        usernameInput.type = 'text';
+        usernameInput.placeholder = 'Username';
+        usernameInput.name = 'username';
+        usernameInput.required = true;
 
-                loginForm.addEventListener('submit', function (e) {
-                    e.preventDefault();
+        const passwordInput = document.createElement('input');
+        passwordInput.type = 'password';
+        passwordInput.placeholder = 'Password';
+        passwordInput.name = 'password';
+        passwordInput.required = true;
 
-                    // Simulate a successful login (you can replace this with actual authentication logic)
-                    const isLoggedIn = true;
+        const submitButton = document.createElement('button');
+        submitButton.type = 'submit';
+        submitButton.textContent = 'Login';
 
-                    if (isLoggedIn) {
-                        // Show the modal with login details
-                        modalUsername.textContent = usernameInput.value;
-                        modalPassword.textContent = passwordInput.value;
-                        loginModal.style.display = 'block';
+        loginForm.appendChild(usernameInput);
+        loginForm.appendChild(passwordInput);
+        loginForm.appendChild(submitButton);
 
-                        // Show the toaster for a successful login
-                        toaster.style.display = 'block';
+        // Create the listing
+        const listing = document.createElement('ul');
+        listing.id = 'listing';
+        listing.style.listStyle = 'none';
+        listing.style.padding = '0';
 
-                        // Clear the form inputs
-                        usernameInput.value = '';
-                        passwordInput.value = '';
-                    }
-                });
+        // Append the elements to the 'app' div
+        const app = document.getElementById('app');
+        app.appendChild(button);
+        app.appendChild(modal);
+        app.appendChild(toaster);
+        app.appendChild(loginForm);
+        app.appendChild(listing);
 
-                app.appendChild(loginForm);
+        // Show the modal when the button is clicked
+        button.addEventListener('click', function () {
+            modal.style.display = 'block';
+        });
+
+        // Close the modal when the "Close" button is clicked
+        modal.addEventListener('click', function (event) {
+            if (event.target.id === 'closeModalBtn') {
+                modal.style.display = 'none';
             }
+        });
 
-            // Initial display: Show the login form
-            displayLoginForm();
-
-            // Close the modal when the "Close" button is clicked
-            closeModalButton.addEventListener('click', function () {
-                loginModal.style.display = 'none';
+        // Simulate fetching data from an API (replace with your API endpoint)
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(item => {
+                    const listItem = document.createElement('li');
+                    listItem.textContent = item.title;
+                    listing.appendChild(listItem);
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
             });
 
-            // Hide the toaster after a few seconds
-            setTimeout(function () {
-                toaster.style.display = 'none';
-            }, 3000);
-        });
+        // Show the toaster
+        toaster.style.display = 'block';
+
+        // Hide the toaster after 3 seconds
+        setTimeout(function () {
+            toaster.style.display = 'none';
+        }, 3000);
+      })
