f324854457ab67c44ced5646a0d9a27244120bee
rameshgagal80 <140626805+rameshgagal80@users.noreply.github.com>
2023-09-21 12:29:24 +0530

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,16 +1,48 @@
-// Embed Script (embed.js)
-document.addEventListener("DOMContentLoaded", function () {
-  const targetDiv = document.getElementById("embeddedContent");
+document.addEventListener('DOMContentLoaded', function () {
+    const app = document.getElementById('testing-script');
+    
+    // Create the login form
+    const loginForm = document.createElement('form');
+    loginForm.id = 'loginForm';
 
-  // Fetch the content from the source website
-  fetch("https://violet-tallulah-83.tiiny.site/")
-    .then((response) => response.text())
-    .then((html) => {
-      const parser = new DOMParser();
-      const sourceDoc = parser.parseFromString(html, "text/html");
-      const embeddableContent = sourceDoc.getElementById("embeddableContent");
+    // Create the username input
+    const usernameInput = document.createElement('input');
+    usernameInput.type = 'text';
+    usernameInput.placeholder = 'Username';
+    usernameInput.name = 'username';
+    usernameInput.required = true;
 
-      // Append the extracted content to the targetDiv
-      targetDiv.appendChild(embeddableContent);
+    // Create the password input
+    const passwordInput = document.createElement('input');
+    passwordInput.type = 'password';
+    passwordInput.placeholder = 'Password';
+    passwordInput.name = 'password';
+    passwordInput.required = true;
+
+    // Create the submit button
+    const submitButton = document.createElement('button');
+    submitButton.type = 'submit';
+    submitButton.textContent = 'Login';
+
+    // Append form elements to the form
+    loginForm.appendChild(usernameInput);
+    loginForm.appendChild(passwordInput);
+    loginForm.appendChild(submitButton);
+
+    // Append the form to the app container
+    app.appendChild(loginForm);
+
+    // Add a form submission event listener
+    loginForm.addEventListener('submit', function (e) {
+        e.preventDefault(); // Prevent the default form submission
+        
+        // Get the form data
+        const formData = new FormData(loginForm);
+        const username = formData.get('username');
+        const password = formData.get('password');
+
+        // You can now make an API call with the username and password
+        // For this example, we'll just display them in an alert
+        alert(`Username: ${username}\nPassword: ${password}`);
     });
 });
